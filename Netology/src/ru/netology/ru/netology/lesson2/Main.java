570092c5e7bb54dd6a99ab9e3cffbb7f6abc3957
Yuliya Ilina <flow987876765@gmail.com>
2022-03-11 21:46:22 +0900

    added

---
+++ Netology/src/ru/netology/ru/netology/lesson2/Main.java
@@ -0,0 +1,19 @@
+package ru.netology.ru.netology.lesson2;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int value1 = scanner.nextInt();
+        System.out.println("Введите второе число: ");
+        int value2 = scanner.nextInt();
+        System.out.println("Введите третье число: ");
+        int value3 = scanner.nextInt();
+        int result = sum (value1, value2, value3);
+        System.out.println("Сумма чисел = " + result);
+    }
+    public static int sum (int value1, int value2, int value3) {
+        return value1 + value2 + value3;
+    }
+}
